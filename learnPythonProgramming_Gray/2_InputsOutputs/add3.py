3107131b51a2137039569b4cf2341ff23c28566b
Federico <fedevb94@hotmail.com>
2022-06-24 17:28:07 -0300

    initial commit

---
+++ learnPythonProgramming_Gray/2_InputsOutputs/add3.py
@@ -0,0 +1,5 @@
+x = int(input('Enter an integer: '))
+y = int(input('Enter another integer: '))
+z = int(input('Enter yet another integer: '))
+
+print('The sum of ', x, ', ', y, ', and ', z, ' is ', x+y+z,'.', sep='') 
